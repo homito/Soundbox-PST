cdc8d122d87bac2767f9b9ceb38ab0cf8cb9e3d7
Yoann <homito-yoann@hotmail.fr>
2023-06-02 14:13:34 +0200

    should work on its own as long as you pair it and manually launch the scripts ( logs, display, metadata)

--- plugins/bluetooth/restartpair.sh
+++ plugins/bluetooth/restartpair.sh
@@ -2,7 +2,3 @@
 for device in $(bluetoothctl devices  | grep -o "[[:xdigit:]:]\{8,17\}"); do
     echo "removing bluetooth device: $device | $(bluetoothctl remove $device)"
 done
-
-echo 2 > /shared/logs/status.txt
-
-reboot -f
